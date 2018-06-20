4d2cbec266377802f8fddc45fec88bb81b797290
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-06-20 14:09:32 +0300

    Added the interface header

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC=gcc
 
-gpio_lib.o:
+gpio_lib.o: gpio_lib.c gpio_lib.h
 	$(CC) -c gpio_lib.c -o $@
 
 clean:
---
+++ gpio.h
@@ -0,0 +1,16 @@
+#ifndef __GPIO_H__
+#define __GPIO_H__
+
+#include "gpio_lib.h"
+
+#define HIGH  1
+#define LOW   0
+
+#define gpioInit() sunxi_gpio_init()
+#define gpioCleanup() sunxi_gpio_cleanup()
+#define digitalWrite(pin, value) sunxi_gpio_input(pin, value) # gpio.write(pin, value)
+#define digitalRead(pin) sunxi_gpio_output(pin) # GPIO::read(pin)
+#define pinMode(pin, direction) gpio.open(pin, direction)
+
+#endif // __GPIO_H__
+
